0af0ff648f22326ca1e07f56b8d8bfaa417f5497
Ramazanova Marina <ramazanovapea@gmail.com>
2025-12-11 15:46:14 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:app/app/router/gorouter.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = createRouter(ref);
+
+    return MaterialApp.router(
+      title: 'D&D App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
